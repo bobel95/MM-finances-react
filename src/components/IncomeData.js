b7188db66fcbe0c8744231d5634363dc2ec45dc8
Iulian Popa <bobel2007@gmail.com>
2021-06-14 18:17:08 +0300

    Add progress bar for income spent

--- src/components/IncomeData.js
+++ src/components/IncomeData.js
@@ -1,30 +1,128 @@
 import React, {useState, useEffect} from 'react';
 import { getUser } from "../api/appUser";
-import {filterLastMonthPayments, groupPaymentsByCategory} from "../util/paymentsUtil";
+import { groupPaymentsByCategory, filterPaymentsByDate } from "../util/paymentsUtil";
+import { Progress } from 'react-sweet-progress';
+import 'react-sweet-progress/lib/style.css';
+import Typography from "@material-ui/core/Typography";
+import Table from "@material-ui/core/Table";
+import TableRow from "@material-ui/core/TableRow";
+import TableBody from "@material-ui/core/TableBody";
+import {TableCell} from "@material-ui/core";
+
+const containerStyle = {
+    display: "flex",
+    flexDirection: "row",
+    width: "60%",
+    alignItems: "center",
+    justifyContent: "center"
+}
+
+const textContainerStyle = {
+    marginLeft: "10px",
+    flex: "1",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+}
+
+const getTheme = percentage => {
+    let trailColor = 'lime';
+    let color = 'green';
+
+    if (percentage > 50) {
+        trailColor = 'yellow';
+        color = 'orange';
+    }
+
+    if (percentage > 80) {
+        trailColor = 'pink';
+        color = 'red';
+    }
+
+    return {
+        active: {
+            symbol: percentage + '%',
+            trailColor: trailColor,
+            color: color
+        }
+    }
+}
 
 const IncomeData = () => {
 
-    const [data, setData] = useState({income: [], payments: []});
+    const [userData, setUserData] = useState(null);
+    const [paymentsData, setPaymentsData] = useState({});
+
+    const [incomeTotal, setIncomeTotal] = useState(0);
+    const [paymentsTotal, setPaymentsTotal] = useState(0);
+
+    const [isLoading, setIsLoading] = useState(true);
+
 
     useEffect(() => {
         getUser()
             .then(res => {
-                setData({
-                    income: res.data.incomeList,
-                    payments: res.data.paymentList
-                });
+                setUserData(res.data);
             })
     }, [])
 
-    let data1 = filterLastMonthPayments(data.payments);
-    data1 = groupPaymentsByCategory(data1);
-    console.log(data1);
+    useEffect(() => {
+        if (userData) {
+            let incomeDate = userData.incomeList[0].date;
+            incomeDate = new Date(incomeDate);
+            incomeDate.setMonth(incomeDate.getMonth() - 1);
 
-    return (
-        <div>
+            let filteredPayments = filterPaymentsByDate(userData.paymentList, incomeDate);
+            filteredPayments = groupPaymentsByCategory(filteredPayments);
 
+            setPaymentsData(filteredPayments)
+            setPaymentsTotal(filteredPayments
+                .reduce((prev, p) => prev + p.value, 0)
+            );
+            setIncomeTotal(userData.incomeList[0].money.amount);
+
+
+            setIsLoading(false);
+        }
+    }, [userData]);
+
+    return isLoading
+        ? (<div>Loading</div>)
+        : (
+        <div style={containerStyle}>
+            <Progress
+                type="circle"
+                strokeWidth={11}
+                width={150}
+                theme={getTheme((100 * paymentsTotal / incomeTotal).toFixed(2))}
+                percent={
+                    (100 * paymentsTotal / incomeTotal).toFixed(2)
+                }
+            />
+            <div style={textContainerStyle}>
+                <Table>
+                    <caption>Resets on {new Date(userData.incomeList[0].date).getDate()}th every month</caption>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell style={{textAlign: "center"}}>
+                                <Typography variant="h6">
+                                    Income: {incomeTotal} RON
+                                </Typography>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell style={{textAlign: "center"}}>
+                                <Typography variant="h6">
+                                    Spent: {paymentsTotal} RON
+                                </Typography>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
         </div>
-    );
+        );
 };
 
 export default IncomeData;
