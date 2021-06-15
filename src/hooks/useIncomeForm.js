3fbd58c3dd6d2dde4aaa15f47aebed9686f579eb
Iulian Popa <bobel2007@gmail.com>
2021-06-15 21:17:34 +0300

    Add update functionality to incomeForm hook

--- src/hooks/useIncomeForm.js
+++ src/hooks/useIncomeForm.js
@@ -1,14 +1,13 @@
 import {useEffect, useState} from "react";
 import {toast} from "react-toastify";
 import incomeFormValidator from "../util/validation/incomeFormValidator";
-import {addIncome} from "../api/income";
+import {addIncome, updateIncome} from "../api/income";
 import {useHistory} from "react-router-dom";
 
-const useIncomeForm = () => {
-
+const useIncomeForm = (isUpdating, data) => {
     const [values, setValues] = useState({
-        date: "",
-        amount: "",
+        date: isUpdating ? data.date : "",
+        amount: isUpdating ? data.amount: "",
     });
     const [errors, setErrors] = useState({ message: "" });
     const [isSubmitting, setIsSubmitting] = useState(false);
@@ -27,31 +26,57 @@ const useIncomeForm = () => {
         setIsSubmitting(true);
     };
 
-    useEffect(() => {
+    const submit = () => {
         if (isSubmitting && Object.keys(errors).length === 0) {
             addIncome(values.amount, values.date)
                 .then(() => {
-                    toast.success(
-                        "Income added!",
-                        {
-                            position: "bottom-center"
-                        });
+                    notifySuccess("Income added!");
                     history.push("/account");
                 })
                 .catch(() => {
-                    toast.error(
-                        "Something went wrong :(",
-                        {
-                            position: "bottom-center"
-                        });
-                    setIsSubmitting(false);
+                    notifyError()
                 });
         }
+    }
+
+    const update = () => {
+        if (isSubmitting && Object.keys(errors).length === 0) {
+            updateIncome(values.amount, values.date, data.id)
+                .then(() => {
+                    notifySuccess("Income updated!");
+                    history.push("/account");
+                })
+                .catch(() => {
+                    notifyError();
+                })
+        }
+    }
+
+    useEffect(() => {
+        isUpdating
+            ? update()
+            : submit()
     }, [errors, isSubmitting]);
+
+
+    const notifySuccess = (text) => {
+        toast.success(
+            `${text}`,
+            {
+                position: "bottom-center"
+            });
+    }
+
+    const notifyError = () => {
+        toast.error(
+            "Something went wrong :(",
+            {
+                position: "bottom-center"
+            });
+        setIsSubmitting(false);
+    }
 
     return { values, handleChange, handleSubmit, errors };
 };
-
-
 
 export default useIncomeForm;
