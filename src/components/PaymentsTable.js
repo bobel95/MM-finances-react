00747e06ee03ea28c117a61fcb5a1229a3e157a7
Iulian Popa <bobel2007@gmail.com>
2021-06-16 15:20:49 +0300

    Add category filter for table view

--- src/components/PaymentsTable.js
+++ src/components/PaymentsTable.js
@@ -1,6 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import { DataGrid } from '@material-ui/data-grid';
 import {formatEnumString} from "../util/stringUtils";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 
 const mainContainerStyle = {
@@ -9,6 +13,7 @@ const mainContainerStyle = {
     height: "100%",
     borderRadius: "1rem",
     display: "flex",
+    flexDirection: "column",
     padding: "2rem 2rem 0 2rem",
     boxShadow: "0px 0px 3px 0px rgba(0,0,0,0.55)",
 }
@@ -17,6 +22,9 @@ const PaymentsTable = (props) => {
 
     const payments = props.payments;
     const [rows, setRows] = useState([]);
+    const [filteredPayments, setFilteredPayments] = useState([...payments]);
+    const [category, setCategory] = useState("All");
+
 
     const columns = [
         { field: 'id', headerName: 'ID', width: 100 },
@@ -31,7 +39,7 @@ const PaymentsTable = (props) => {
     }
 
     const createRows = () => {
-        setRows(payments.map(p =>
+        setRows(filteredPayments.map(p =>
             createData(
                 p.id,
                 p.date,
@@ -40,11 +48,52 @@ const PaymentsTable = (props) => {
                 p.money.currency
             )));
     }
-    useEffect(createRows, [payments]);
+
+    const handleCategoryChange = (e) => {
+        setCategory(e.target.value);
+    }
+
+    useEffect(() => {
+        setFilteredPayments(
+            category !== "All"
+                ? payments.filter(p => p.paymentCategory === category)
+                : payments
+        );
+    }, [category]);
+
+
+    useEffect(createRows, [filteredPayments]);
 
     return (
         <div style={mainContainerStyle}>
-            <div style={{ height: 400, width: '100%', }}>
+            <form>
+                <FormControl
+                    // className={classes.formControl}
+                    // fullWidth
+                >
+                    <InputLabel>Category</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        onChange={handleCategoryChange}
+                        value={category}
+                        required
+                    >
+                        <MenuItem value={"All"}>All</MenuItem>
+                        {payments
+                            .map(p => p.paymentCategory)
+                            .filter((category, idx, self) => self.indexOf(category) === idx)
+                            .map((category, idx) =>
+                            (<MenuItem value={category} key={idx}>
+                                {formatEnumString(category)}
+                            </MenuItem>)
+                        )}
+
+                    </Select>
+                </FormControl>
+            </form>
+
+            <div style={{ height: 400, width: '100%'}}>
                 <DataGrid rows={rows} columns={columns} pageSize={5} />
             </div>
         </div>
