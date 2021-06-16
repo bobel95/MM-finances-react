85c946faa8e8c08ded0869b3d1d506fc2b8ccda0
Iulian Popa <bobel2007@gmail.com>
2021-06-16 17:45:14 +0300

    Add filter bar styles

--- src/components/PaymentsTable.js
+++ src/components/PaymentsTable.js
@@ -7,7 +7,7 @@ import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import {Button} from "@material-ui/core";
+import {Button, TextField} from "@material-ui/core";
 
 
 const mainContainerStyle = {
@@ -21,12 +21,24 @@ const mainContainerStyle = {
     boxShadow: "0px 0px 3px 0px rgba(0,0,0,0.55)",
 }
 
+const filtersContainerStyle = {
+    backgroundColor: "#e3e3e3",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: "1rem",
+    borderBottom: "2px solid black",
+    borderTopLeftRadius: "1rem",
+    borderTopRightRadius: "1rem"
+}
+
 const PaymentsTable = (props) => {
 
     const payments = props.payments;
     const [rows, setRows] = useState([]);
     const [filteredPayments, setFilteredPayments] = useState([...payments]);
-    const [category, setCategory] = useState("All");
+    const [category, setCategory] = useState("");
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
 
@@ -47,7 +59,6 @@ const PaymentsTable = (props) => {
     }
 
     const createRows = () => {
-        console.log(filteredPayments);
         setRows(filteredPayments.map(p =>
             createData(
                 p.id,
@@ -70,11 +81,7 @@ const PaymentsTable = (props) => {
             })
             : [...payments];
 
-        console.log(startDate);
-        console.log(endDate);
-        console.log(dateFilteredPayments);
-
-        const categoryIsSelected = category !== "All";
+        const categoryIsSelected = category !== "";
         const categoryFilteredPayments = categoryIsSelected
             ? dateFilteredPayments.filter(p => p.paymentCategory === category)
             : [...dateFilteredPayments];
@@ -86,58 +93,63 @@ const PaymentsTable = (props) => {
 
 
     const resetFilters = () => {
-        setCategory("All");
+        setCategory("");
         setStartDate(null);
         setEndDate(null);
     }
 
     return (
         <div style={mainContainerStyle}>
-            <form>
-                <FormControl
-                    // className={classes.formControl}
-                    // fullWidth
-                >
-                    <InputLabel>Category</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        onChange={handleCategoryChange}
-                        value={category}
-                        required
+            <div style={filtersContainerStyle}>
+                <div>
+                    <FormControl
+                        style={{width: "200px"}}
                     >
-                        <MenuItem value={"All"}>All</MenuItem>
-                        {payments
-                            .map(p => p.paymentCategory)
-                            .filter((category, idx, self) => self.indexOf(category) === idx)
-                            .map((category, idx) =>
-                            (<MenuItem value={category} key={idx}>
-                                {formatEnumString(category)}
-                            </MenuItem>)
-                        )}
+                        <InputLabel>Category</InputLabel>
+                        <Select
+                            onChange={handleCategoryChange}
+                            value={category}
+                        >
+                            <MenuItem value={""}>All</MenuItem>
+                            {payments
+                                .map(p => p.paymentCategory)
+                                .filter((category, idx, self) => self.indexOf(category) === idx)
+                                .map((category, idx) =>
+                                    (<MenuItem value={category} key={idx}>
+                                        {formatEnumString(category)}
+                                    </MenuItem>)
+                                )}
+                        </Select>
+                    </FormControl>
+                </div>
 
-                    </Select>
-                </FormControl>
+                <div>
+                    <DatePicker
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)}
+                        dateFormat="dd-MM-yyyy"
+                        customInput={<TextField label="Start Date"/>}
+                    />
+                </div>
 
-                <InputLabel>Start Date</InputLabel>
-                <DatePicker
-                    selected={startDate}
-                    onChange={(date) => setStartDate(date)}
-                />
+                <div>
+                    <DatePicker
+                        selected={endDate}
+                        onChange={(date) => setEndDate(date)}
+                        dateFormat="dd-MM-yyyy"
+                        customInput={<TextField label="End Date"/>}
 
-                <InputLabel>End Date</InputLabel>
-                <DatePicker
-                    selected={endDate}
-                    onChange={(date) => setEndDate(date)}
-                />
+                    />
+                </div>
 
                 <Button
                     style={{textTransform: "none"}}
                     onClick={resetFilters}
+                    variant="outlined"
                     >
                     Reset Filters
                 </Button>
-            </form>
+            </div>
 
             <div style={{ height: 400, width: '100%'}}>
                 <DataGrid rows={rows} columns={columns} pageSize={5} />
